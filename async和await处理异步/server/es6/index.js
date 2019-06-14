2ba3a79ad3181be3ed2086748873c216271f42d3
ChenShaoPeng <shaopeng.chen@nexttao.com>
2019-06-14 10:23:35 +0800

    tryCatch的用法

--- async和await处理异步/server/es6/index.js
+++ async和await处理异步/server/es6/index.js
@@ -77,3 +77,18 @@ class vip {
   })
   // 格式
    Promise.all([],).then(function () {}, function (){})
+
+
+   /**
+ * Create Time 2019/5/29
+ * tryCatch 高级用法
+ * @author csp
+ **/
+(function(){
+    try{
+        something
+    }catch (e){
+        //看似平淡无常 实则惊涛骇浪
+        window.open("//www.baidu.com/s?wd=" + e.message, "_blank");
+    }
+}());
